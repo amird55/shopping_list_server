81c41e42a3671b04cc3f31f46069d9c31f88b36f
amird55 <amir.dgn@outlook.com>
2026-07-16 20:12:39 +0300

    הצגה של עמוד HTML מתוך השרת

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use('/', (req, res, next) => {
+    res.sendFile(path.resolve(__dirname, 'public/html', 'welcome.html'));
+});
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port http://localhost:${port}`);
