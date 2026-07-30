ab1d80f2eee14fa3ed92534bbc7600f1e735eb94
amird55 <amir.dgn@outlook.com>
2026-07-30 20:08:37 +0300

    הוספת ספריית body-parser כדי לעבוד עם BODY שנשלח מה-insomnia

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
--- routers/categ_R.js
+++ routers/categ_R.js
@@ -12,6 +12,7 @@ router.get("/Add", [], (req, res) => {
 });
 
 router.post("/Create", [], (req, res) => {
-    res.status(200).json({message:"created"});
+    const {name} = req.body;
+    res.status(200).json({message:"created",name:name});
 });
 
